f091edbe5ce97ca5d58f2d7368bd60e5e67f3619
Hugo Santos <santoshugo527@gmail.com>
2026-08-20 10:28:54 -0300

    exceptions

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/error/test/StackOverflowtest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Oexception.error.test;
+
+public class StackOverflowtest01 {
+    public static void main(String[] args) {
+        recursividade();
+    }
+
+    public static void recursividade(){
+        recursividade();
+    }
+
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/SobrescritaComExceptionTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
+
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Pessoa;
+
+public class SobrescritaComExceptionTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        Funcionario funcionario = new Funcionario();
+        funcionario.salvar();
+
+    }
+}
