d809c4b9f5284e51aa25c3f7dc91b626980c4be6
Hugo Santos <santoshugo527@gmail.com>
2026-08-27 10:27:58 -0300

    legacy date class

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/CalendarTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTest01 {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+
+        if (c.getFirstDayOfWeek() == Calendar.SUNDAY){
+            System.out.println("Domingo é o primeiro dia da semana");
+        }
+        System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+        Date date = c.getTime();
+        System.out.println(date);
+    }
+}
