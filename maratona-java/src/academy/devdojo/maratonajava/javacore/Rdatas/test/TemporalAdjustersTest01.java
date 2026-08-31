e701084c90cb34e25ee3d73f636d259031abed1c
Hugo Santos <santoshugo527@gmail.com>
2026-08-31 09:41:36 -0300

    instant, duration, period and temporaladjusters

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/TemporalAdjustersTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDate;
+
+public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        now = now.plusDays(2);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+    }
+}
