eb97ba2f82082f72cc67775575309ca1b26760a3
Hugo Santos <santoshugo527@gmail.com>
2026-09-14 09:08:48 -0300

    last regex content

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcharTest05.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcharTest05.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcharTest04 {
+public class PatternMatcharTest05 {
     public static void main(String[] args) {
 
         // \d = Todos os dígitos
@@ -21,8 +21,8 @@ public class PatternMatcharTest04 {
         // |
         // $
         // .
-        String regex = "0[xX] ([0-9a-fA-F])+(\\s|&)";
-        String texto = "12 0x 0X 0xFFABC 0x10G 0x1";
+        String regex = "([a-zA-Z0-9\\._-])+@([a-zA-Z])+(\\.@([a-zA-Z])+)+";
+        String texto = "luffy@hotmail.com, 123jotaro@gmail.com, #@!zoro@mail.br, teste@gmail.com, sakura@mail";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
         System.out.println("texto: " + texto);
